2f33f64571850db5f1fe2c1c3ce7b9b38441036a
asmartcowmedia <asmartcowmedia@gmail.com>
2022-07-29 02:48:47 -0500

    updated jumping
    - implemented double jump
    - fixed is grounded issues
    - added adjustments to code for jump variables

--- Assets/Dev_Lynx/Scripts/Player/Movement.cs
+++ Assets/Dev_Lynx/Scripts/Player/Movement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -11,8 +12,17 @@ public class Movement : MonoBehaviour
 
         [Header("Input Variable Fields - Jumping")]
         [SerializeField] float jumpHeight;
+        [SerializeField] private float groundDetectRayLength;
         [SerializeField] GameObject groundCheck;
         [SerializeField] LayerMask ground;
+        [SerializeField] private bool canDoubleJump;
+        [SerializeField] private int maxJumpAmount;
+        [SerializeField] private float afterFirstJumpMultiplier;
+        [SerializeField] private int timesJumped;
+
+        [Header("Gravity")] 
+        [SerializeField] private float gravityScale = 1f;
+        public static float globalGravity = -9.81f; 
         
         bool jump;
         bool isGrounded;
@@ -26,27 +36,28 @@ public class Movement : MonoBehaviour
 
         void Start() {
             rb = GetComponent<Rigidbody>();
+            rb.useGravity = false;
         }
 
         void Update(){
-            HandleAllInput();
-            isGrounded = Physics.CheckSphere(groundCheck.transform.position, .2f, ground);
+            HandleMovementInput();
+            HandleJumpInput();
+            isGrounded = Physics.Raycast(groundCheck.transform.position, Vector3.down, groundDetectRayLength, ground);
         }
 
         void FixedUpdate() {
             Move();
-            if(rb.velocity.y < .75f * jumpHeight || !jump) {
-                rb.velocity += Vector3.up * Physics.gravity.y * Time.fixedDeltaTime * 5f;
-            }
+            Gravity();
         }
 
         #endregion
 
         #region Private Methods
 
-        private void HandleAllInput() {
-            HandleMovementInput();
-            HandleJumpInput();
+        private void Gravity()
+        {
+            Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+            rb.AddForce(gravity, ForceMode.Acceleration);
         }
 
         private void HandleMovementInput() {
@@ -59,8 +70,26 @@ public class Movement : MonoBehaviour
 
         private void HandleJumpInput(){
             jump = Input.GetButtonDown("Jump");
+
+            if (isGrounded)
+            {
+                canDoubleJump = true;
+                timesJumped = 0;
+            }
+            
             if(jump && isGrounded) {
-                Jump();
+                Jump(jumpHeight);
+                timesJumped++;
+            }
+            if (jump && !isGrounded && canDoubleJump && timesJumped < maxJumpAmount)
+            {
+                var height = jumpHeight * afterFirstJumpMultiplier;
+                Jump(height);
+                timesJumped++;
+            }
+            else if (timesJumped >= maxJumpAmount)
+            {
+                canDoubleJump = false;
             }
         }
 
@@ -68,9 +97,10 @@ public class Movement : MonoBehaviour
             rb.velocity = moveDirection;
         }
 
-        private void Jump(){
-            
-            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        private void Jump(float height)
+        {
+            rb.AddForce(Vector3.up * height, ForceMode.Impulse);
+            timesJumped++;
         }
 
         #endregion
